e5a97a273660b0da92e8b1d80a219289ac2fe64a
Abhijeet Singh Khangarot <abhijeet40308@gmail.com>
2020-12-03 02:11:55 +0530

    add types to datasource

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import * as GQL from "graphql";
 import React, { useEffect, useState } from "react";
 import Tree from "./components/Tree";
 import { SCHEMA } from "./constants";
-import { getTree } from "./utils";
+import { getTree, getType } from "./utils";
 import "./styles.css";
 
 const { buildClientSchema } = GQL;
@@ -11,16 +11,16 @@ export default function App() {
   return (
     <div>
       <h1>from real schema</h1>
-      <RSP source={SCHEMA}/>
+      <RSP source={SCHEMA} />
     </div>
   );
 }
 
-const RSP = ({source}) => {
+const RSP = ({ source }) => {
   const [datasource, setDatasource] = useState([]);
   const [schema, setSchema] = useState();
   useEffect(() => {
-    if(!source)return
+    if (!source) return;
     const schema = buildClientSchema(source);
     window.SCHEMA = schema;
     window.GQL = GQL;
@@ -28,22 +28,24 @@ const RSP = ({source}) => {
   }, [source]);
 
   useEffect(() => {
-    // TODO filter other types and add to this array
-    schema &&
-      setDatasource([
-        {
-          name: "query_root",
-          children: getTree(schema, "QUERY")
-        },
-        {
-          name: "mutation_root",
-          children: getTree(schema, "MUTATION")
-        }
-      ]);
+    if (!schema) return;
+
+    const types = getType(schema);
+    setDatasource([
+      {
+        name: "query_root",
+        children: getTree(schema, "QUERY")
+      },
+      {
+        name: "mutation_root",
+        children: getTree(schema, "MUTATION")
+      },
+      ...types
+    ]);
   }, [schema]);
   return schema && datasource && datasource.length ? (
     <Tree {...{ datasource, schema, setDatasource }} />
   ) : (
-      "Loading..."
-    );
+    "Loading..."
+  );
 };
